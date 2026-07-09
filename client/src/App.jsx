b439c09f0cac6ec16f806fc51ad53f208d6dc398
SirShabluli <eyalmordechai@gmail.com>
2026-07-09 23:11:42 +0700

    Add age gate (18+) to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,24 @@ import Home from './pages/Home';
 import AdminLogin from './admin/AdminLogin';
 import AdminDashboard from './admin/AdminDashboard';
 import ProfileForm from './admin/ProfileForm';
+import AgeGate from './components/AgeGate';
 import './i18n';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/profiles/new" element={<ProfileForm />} />
-        <Route path="/admin/profiles/:id/edit" element={<ProfileForm />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <AgeGate />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/profiles/new" element={<ProfileForm />} />
+          <Route path="/admin/profiles/:id/edit" element={<ProfileForm />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
